f050e84d9e3644a8c8665feb32954cb37c3ffc1a
SatioO <vaibhav.satam29991@gmail.com>
2018-08-04 17:34:56 +0530

    feat(accordian): added lazy loading of components

--- src/components/Accordion/config.js
+++ src/components/Accordion/config.js
@@ -1,24 +1,22 @@
-import React from "react";
-
 export default [
     {
         id: "BookCover",
         title: "Book Cover",
-        template: () => <h1>Book Cover Component </h1>
+        template: () => import("../Templates/BookCover")
     },
     {
         id: "ProfilePhoto",
         title: "Profile Photo",
-        template: () => <h1>Profile Photo Component </h1>
+        template: () => import("../Templates/ProfilePhoto")
     },
     {
         id: "ContactDetails",
         title: "Contact Details",
-        template: () => <h1>Contact Details Component </h1>
+        template: () => import("../Templates/ContactDetails")
     },
     {
         id: "BookDetails",
         title: "Book Details",
-        template: () => <h1>Book Details Component </h1>
+        template: () => import("../Templates/BookDetails")
     }
 ];
--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,51 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 import config from "./config";
+import Loadable from "react-loadable";
 
-export default () => (
-    <div className="col col-md-4">
-        <div className="accordion" id="accordionCoverPageEditor">
-            {config.map((accordion, i) => {
-                return (
-                    <div className="card" key={i}>
-                        <div className="card-header" id="headingOne">
-                            <button
-                                className="btn btn-link"
-                                type="button"
-                                data-toggle="collapse"
-                                data-target={"#" + accordion.id}
-                                aria-expanded="true"
-                                aria-controls="bookCover"
+let LoadableBar = null;
+
+export default class Accordion extends Component {
+    handleClick = target => {
+        requestAnimationFrame(() => {
+            if (this.refs[target.id].classList.contains("show")) {
+                this.refs[target.id].classList.remove("show");
+            } else {
+                Object.keys(this.refs).forEach(ref => {
+                    this.refs[ref].classList.remove("show");
+                });
+                this.refs[target.id].classList.add("show");
+            }
+
+            LoadableBar = Loadable({
+                loader: target.template,
+                loading: () => {
+                    return <div>Loading...</div>;
+                }
+            });
+            this.setState({});
+        });
+    };
+    render() {
+        return (
+            <div className="col col-md-4">
+                <div className="accordion" id="accordionCoverPageEditor">
+                    {config.map((accordion, i) => (
+                        <div className="card" key={i}>
+                            <div className="card-header" id="headingOne">
+                                <button
+                                    className="btn btn-link"
+                                    type="button"
+                                    data-toggle="collapse"
+                                    onClick={() => this.handleClick(accordion)}
+                                    aria-expanded="true"
+                                    aria-controls="bookCover"
+                                >
+                                    {accordion.title}
+                                    <div className="accordion_icon">
+                                        <span className="minus_icon">
+                                            <i className="fas fa-minus" />
+                                        </span>
+                                        <span className="plus_icon">
+                                            <i className="fas fa-plus" />
+                                        </span>
+                                    </div>
+                                </button>
+                            </div>
+                            <div
+                                ref={accordion.id}
+                                className="collapse"
+                                aria-labelledby="headingOne"
+                                data-parent="#accordionCoverPageEditor"
                             >
-                                {accordion.title}
-                                <div className="accordion_icon">
-                                    <span className="minus_icon">
-                                        <i className="fas fa-minus" />
-                                    </span>
-                                    <span className="plus_icon">
-                                        <i className="fas fa-plus" />
-                                    </span>
-                                </div>
-                            </button>
-                        </div>
-                        <div
-                            id={accordion.id}
-                            className="collapse"
-                            aria-labelledby="headingOne"
-                            data-parent="#accordionCoverPageEditor"
-                        >
-                            <div className="card-body accordion_scrollbar">
-                                <div className="book_cover_wrap text-center">
-                                    <div className="row">
-                                        {accordion.template && (
-                                            <accordion.template />
-                                        )}
+                                <div className="card-body accordion_scrollbar">
+                                    <div className="book_cover_wrap text-center">
+                                        <div className="row">
+                                            {LoadableBar && <LoadableBar />}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                );
-            })}
-        </div>
-    </div>
-);
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
