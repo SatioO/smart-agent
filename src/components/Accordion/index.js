0ff292c037b612ae40a388f67f814dcb91fc2d07
sonia <sonia.jain@accionlabs.com>
2018-08-06 17:06:43 +0530

    grid changes

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -37,16 +37,16 @@ export default class Accordion extends Component {
                                     type="button"
                                     data-toggle="collapse"
                                     onClick={() => this.handleClick(accordion)}
-                                    aria-expanded="true"
+                                    aria-expanded="false"
                                     aria-controls="bookCover"
                                 >
                                     {accordion.title}
                                     <div className="accordion_icon">
                                         <span className="minus_icon">
-                                            <i className="fas fa-minus" />
+                                            <i className="fa fa-minus" />
                                         </span>
                                         <span className="plus_icon">
-                                            <i className="fas fa-plus" />
+                                            <i className="fa fa-plus" />
                                         </span>
                                     </div>
                                 </button>
