71d90bb3d05d88744d8b8d83abdb508c03f1c4ac
neha.tiwari <neha.tiwari@accionlabs.com>
2018-08-06 15:24:52 +0530

    feat(ConfigFixed)

--- src/components/Accordion/config.js
+++ src/components/Accordion/config.js
@@ -1,40 +1,28 @@
 // import React from 'react';
-import BookCover from "../LeftSideAccordions/BookCover";
-import ProfilePhoto from "../LeftSideAccordions/ProfilePhoto";
-import ContactDetails from "../LeftSideAccordions/ContactDetails";
-import BookDetails from "../LeftSideAccordions/BookDetails";
+// import BookCover from "../LeftSideAccordions/BookCover";
+// import ProfilePhoto from "../LeftSideAccordions/ProfilePhoto";
+// import ContactDetails from "../LeftSideAccordions/ContactDetails";
+// import BookDetails from "../LeftSideAccordions/BookDetails";
 
 export default [
     {
         id: "BookCover",
         title: "Book Cover",
-        heading: "headingOne",
-        dataTarget: "bookCover",
-        // template: () => <h1>Book Cover Component </h1>,
-        template: BookCover
+        template: () => import("../LeftSideAccordions/BookCover")
     },
     {
         id: "ProfilePhoto",
         title: "Profile Photo",
-        heading: "headingTwo",
-        dataTarget: "profilePhoto",
-        // template: () => <h1>Profile Photo Component </h1>
-        template: ProfilePhoto
+        template: () => import("../LeftSideAccordions/ProfilePhoto")
     },
     {
         id: "ContactDetails",
         title: "Contact Details",
-        heading: "headingThree",
-        dataTarget: "contactDetails",
-        // template: () => <h1>Contact Details Component </h1>,
-        template: ContactDetails
+        template: () => import("../LeftSideAccordions/ContactDetails")
     },
     {
         id: "BookDetails",
         title: "Book Details",
-        eading: "headingFour",
-        dataTarget: "bookDetails",
-        // template: () => <h1>Book Details Component </h1>,
-        template: BookDetails
+        template: () => import("../LeftSideAccordions/BookDetails")
     }
 ];
