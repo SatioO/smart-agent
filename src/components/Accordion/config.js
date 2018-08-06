1e799978bb97af46c0fb84461ab5261ce044e22d
neha.tiwari <neha.tiwari@accionlabs.com>
2018-08-06 14:58:54 +0530

    feat(LeftSideAccorion)

--- src/components/Accordion/config.js
+++ src/components/Accordion/config.js
@@ -1,5 +1,8 @@
-import React from "react";
+// import React from 'react';
 import BookCover from "../LeftSideAccordions/BookCover";
+import ProfilePhoto from "../LeftSideAccordions/ProfilePhoto";
+import ContactDetails from "../LeftSideAccordions/ContactDetails";
+import BookDetails from "../LeftSideAccordions/BookDetails";
 
 export default [
     {
@@ -15,20 +18,23 @@ export default [
         title: "Profile Photo",
         heading: "headingTwo",
         dataTarget: "profilePhoto",
-        template: () => <h1>Profile Photo Component </h1>
+        // template: () => <h1>Profile Photo Component </h1>
+        template: ProfilePhoto
     },
     {
         id: "ContactDetails",
         title: "Contact Details",
         heading: "headingThree",
         dataTarget: "contactDetails",
-        template: () => <h1>Contact Details Component </h1>
+        // template: () => <h1>Contact Details Component </h1>,
+        template: ContactDetails
     },
     {
         id: "BookDetails",
         title: "Book Details",
         eading: "headingFour",
         dataTarget: "bookDetails",
-        template: () => <h1>Book Details Component </h1>
+        // template: () => <h1>Book Details Component </h1>,
+        template: BookDetails
     }
 ];
